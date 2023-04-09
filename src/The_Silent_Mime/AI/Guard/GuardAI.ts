3a84a42ba7cbef279fa1feeea6cd772ad1c1b973
Winston Riggs <winston.riggs@stonybrook.edu>
2023-04-09 15:00:47 -0400

    Guard Pathfinding

--- src/The_Silent_Mime/AI/Guard/GuardAI.ts
+++ src/The_Silent_Mime/AI/Guard/GuardAI.ts
@@ -3,13 +3,16 @@ import AI from "../../../Wolfie2D/DataTypes/Interfaces/AI";
 import GameNode from "../../../Wolfie2D/Nodes/GameNode";
 import GuardActor from "../../Actors/GuardActor";
 import { Patrol, Chase, GuardStateType } from "../Guard/GuardStates/GuardState";
+
 export default class GuardAI extends StateMachineAI implements AI {
   public owner: GuardActor;
 
   public initializeAI(owner: GuardActor, config: Record<string, any>): void {
     this.owner = owner;
+
     this.addState(GuardStateType.CHASE, new Chase(this, this.owner));
     this.addState(GuardStateType.PATROL, new Patrol(this, this.owner));
+    
     this.initialize(GuardStateType.PATROL);
   }
 }
--- src/The_Silent_Mime/AI/Guard/GuardStates/Patrol.ts
+++ src/The_Silent_Mime/AI/Guard/GuardStates/Patrol.ts
@@ -1,11 +1,33 @@
+import Vec2 from "../../../../Wolfie2D/DataTypes/Vec2";
 import GuardState from "./GuardState";
 
 export default class Patrol extends GuardState {
-  public onEnter(options: Record<string, any>): void {}
+  protected targets: Vec2[] = []; 
+  protected curTarget: Vec2;
+  protected curIndex = 0;
+
+  public onEnter(options: Record<string, any>): void {
+    this.targets.push(new Vec2(350, 675));
+    this.targets.push(new Vec2(350, 275));
+    this.targets.push(new Vec2(675, 275));
+    this.targets.push(new Vec2(675, 675));
+
+    this.curIndex = Math.floor(Math.random() * 3);
+
+    this.curTarget = this.targets[this.curIndex];
+  }
 
   public update(deltaT: number): void {
-    super.update(deltaT);
-    this.parent.owner._velocity.x += 1;
+    if (this.parent.owner.position.x == this.curTarget.x && this.parent.owner.position.y == this.curTarget.y) {
+      this.curIndex === 3 ? 0 : this.curIndex++;
+    }
+
+    this.curTarget = this.targets[this.curIndex];
+
+    let x = this.parent.owner.position.x - this.curTarget.x;
+    let y = this.parent.owner.position.y - this.curTarget.y;
+
+    this.parent.owner.move(new Vec2(x > 0 ? -1 : x < 0 ? 1 : 0, y > 0 ? -1 : y < 0 ? 1 : 0));
   }
 
   public onExit(): Record<string, any> {
